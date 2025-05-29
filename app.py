e776360547ca2972cd4570db52d9ba381fa0b732
Violet Vivirand <purpleslovemail@gmail.com>
2025-05-29 11:51:16 +0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+import gradio as gr
+import libtorrent as lt
+import urllib.parse
+import os
+
+def torrent_to_magent(filepaths):
+    magnet_links = []
+    for filepath in filepaths:
+        """Convert a .torrent file to a magnet link with all metadata"""
+        info = lt.torrent_info(filepath)
+        
+        # Start with info hash (required)
+        magnet_link = f"magnet:?xt=urn:btih:{info.info_hash()}"
+        
+        # Add display name
+        if info.name():
+            magnet_link += f"&dn={urllib.parse.quote(info.name())}"
+        
+        # Add trackers
+        for tracker in info.trackers():
+            magnet_link += f"&tr={urllib.parse.quote(tracker.url)}"
+        
+        # Add web seeds
+        for seed in info.web_seeds():
+            magnet_link += f"&ws={urllib.parse.quote(seed.url)}"
+        
+        magnet_links.append(magnet_link)
+    
+    return (os.linesep).join(magnet_links)
+
+
+demo = gr.Interface(
+    fn=torrent_to_magent,
+    inputs=gr.File(label="Upload .torrent file", file_count="multiple", file_types=[".torrent"]),
+    outputs=[gr.Textbox(label="Magnet Link", show_copy_button=True, max_lines=10,)],
+    title="Torrent to Magnet Link Converter",
+    flagging_mode="never",
+)
+
+demo.launch()
